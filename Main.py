623b89d194cf0931d253ed0e8063d1e7e26e325b
jybpan <junyubpan@gmail.com>
2026-01-20 23:09:25 -0700

    Trying Q2

--- Main.py
+++ Main.py
@@ -11,3 +11,22 @@ def consecutiveMultiplication(multThreshold):
     print("Final Product: ", finalProduct, "Integer Exceeding Threshold: ", curNumber+1)
 
 consecutiveMultiplication(100)
+
+# Question 2
+
+def nestedDict(strList):
+    analyzedDict = None
+
+    for i in strList:
+        length = len(i)
+        
+        if length % 2 == 0:
+            parity = "even"
+        else:
+            parity = "odd"
+
+    #TODO: Turn the calculated things into a nested dict.
+    # Turn each value of the list into an empty dictionary, and then iterate through it, 
+    # and then update the key with values that correspond to the parity and length.
+
+    return analyzedDict
