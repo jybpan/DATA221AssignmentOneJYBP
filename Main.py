4259d146d12015b6fb0ff471bb2336ad3c0b2749
jybpan <junyubpan@gmail.com>
2026-01-21 12:42:54 -0700

    Rewrote it, and started with the outer nest of the dictionary.

--- Main.py
+++ Main.py
@@ -10,23 +10,25 @@ def consecutiveMultiplication(multThreshold):
 
     print("Final Product: ", finalProduct, "Integer Exceeding Threshold: ", curNumber+1)
 
-consecutiveMultiplication(100)
-
 # Question 2
 
 def nestedDict(strList):
-    analyzedDict = None
+    analyzedDict = {}
+    listStats = {}
 
     for i in strList:
         length = len(i)
-        
-        if length % 2 == 0:
+
+        if(length % 2 == 0):
             parity = "even"
         else:
             parity = "odd"
 
-    #TODO: Turn the calculated things into a nested dict.
-    # Turn each value of the list into an empty dictionary, and then iterate through it, 
-    # and then update the key with values that correspond to the parity and length.
+        listStats = {"Length":length, "Parity":parity}
 
-    return analyzedDict
+    print(listStats)
+    print(analyzedDict)
+
+test = ["Data", "Science"]
+
+nestedDict(test)
